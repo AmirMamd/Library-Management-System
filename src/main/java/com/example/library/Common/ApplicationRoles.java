4e3dcda6d406ddb9ab70e6d8d7e854fb73669718
AmirMamd <amirr.mamdouh@gmail.com>
2024-08-14 16:25:22 +0300

    first commit

---
+++ src/main/java/com/example/library/Common/ApplicationRoles.java
@@ -0,0 +1,19 @@
+package com.example.library.Common;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ApplicationRoles {
+    public static final String View_Patrons = "ROLE_View_Patrons";
+    public static final String Edit_Patrons = "ROLE_Edit_Patrons";
+    public static final String Add_Patrons = "ROLE_Add_Patrons";
+    public static final String Delete_Patrons = "ROLE_Delete_Patrons";
+
+    public static final String View_Books = "ROLE_View_Books";
+    public static final String Edit_Books = "ROLE_Edit_Books";
+    public static final String Add_Books = "ROLE_Add_Books";
+    public static final String Delete_Books = "ROLE_Delete_Books";
+
+    public static final String Borrow_Book = "ROLE_Borrow_Book";
+    public static final String Return_Book = "ROLE_Return_Book";
+}
